5a35377eeaf1b1f56e774b26f03aaf1004682f39
hengruili <hengruili@users.noreply.github.com>
2020-11-23 19:01:52 +1100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Scissors)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+})
+basic.forever(function () {
+	
+})
